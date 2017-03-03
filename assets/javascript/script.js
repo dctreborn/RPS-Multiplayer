40df7cf39d41433d1c4dea9828e2c9dc357cad0e
Rukaroa <rukaroa@gmail.com>
2017-03-03 17:25:02 -0600

    base html and skeleton functions

---
+++ assets/javascript/script.js
@@ -0,0 +1,33 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC4Fl4hh0CHqkgCZYIien2m0HRZUI81TZY",
+    authDomain: "rukarps.firebaseapp.com",
+    databaseURL: "https://rukarps.firebaseio.com",
+    storageBucket: "rukarps.appspot.com",
+    messagingSenderId: "96724550699"
+  };
+  firebase.initializeApp(config);
+
+  var message;
+  var player1;
+  var player2;
+  var choices;
+  var p1Wins;
+  var p2Wins;
+  var p1Losses;
+  var p2Losses;
+
+//display player message in rps panel; to be clicked and taken as choice
+  function displayChoices(){
+
+  }
+
+//display chat message
+  function displayMessage(){
+
+  }
+
+//select RPS image
+  function selectRPS() {
+
+  }
